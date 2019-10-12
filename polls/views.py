fa345f6ed651d8ecf327fb4168dca0af39e66264
Jason Woo <jwoo@nyu.edu>
2019-10-12 15:51:28 -0400

    Formatting edits

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
 
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -29,11 +30,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(
-            request,
-            'polls/detail.html', 
-            {'question': question, 'error_message': "You didn't select a choice."},
-        )
+        return render(request, 'polls/detail.html',{'question': question, 'error_message': "You didn't select a choice."},)
     else:
         selected_choice.votes += 1
         selected_choice.save()
