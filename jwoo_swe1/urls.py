826c622d4827613c0d4db1db70493df1f26a4364
Jason Woo <jwoo@nyu.edu>
2019-10-12 16:21:53 -0400

    Formatting edits

--- jwoo_swe1/urls.py
+++ jwoo_swe1/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', include('polls.urls')),
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path("", RedirectView.as_view(url="polls/")),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
