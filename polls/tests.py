3ea12c507834527eddc6698248ae2edfcd4e6e53
Jason Woo <jwoo@nyu.edu>
2019-10-14 14:24:31 -0400

    Formatting Edits

--- polls/tests.py
+++ polls/tests.py
@@ -6,7 +6,6 @@ from polls.models import Question
 
 
 class QuestionModelTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
